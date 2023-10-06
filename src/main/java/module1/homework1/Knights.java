adf63cca60aaa71f944bc0bb677715d9b18700b5
not-Whale <rezepinn@yandex.ru>
2023-10-06 09:34:47 +0400

    Добавил решение упражнения 8

---
+++ src/main/java/module1/homework1/Knights.java
@@ -0,0 +1,43 @@
+package module1.homework1;
+
+public class Knights {
+    private static final int n = 8;
+    private static final int m = 8;
+
+    private Knights() {}
+
+    public static boolean knightBoardCapture(int[][] board) {
+        if (board.length != n || board[0].length != m) {
+            return false;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 0) {
+                    continue;
+                }
+
+                if (isBeaten(i - 1, j - 2, board)
+                    || isBeaten(i - 1, j + 2, board)
+                    || isBeaten(i - 2, j - 1, board)
+                    || isBeaten(i - 2, j + 1, board)
+                    || isBeaten(i + 1, j - 2, board)
+                    || isBeaten(i + 1, j + 2, board)
+                    || isBeaten(i + 2, j - 1, board)
+                    || isBeaten(i + 2, j + 1, board)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isBeaten(int x, int y, int[][]board) {
+        return (isOnBoard(x, y) && board[x][y] == 1);
+    }
+
+    private static boolean isOnBoard(int x, int y) {
+        return (x >= 0 && x < n && y >= 0 & y < m);
+    }
+}
