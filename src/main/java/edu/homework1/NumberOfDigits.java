9aa5c0822a2de7b8a05bf5696a331ff6b1d29c3e
not-Whale <rezepinn@yandex.ru>
2023-10-07 21:42:50 +0400

    Поставил SupressWarning на MagicNumber, чтобы checkstyle не ругался на десятки

--- src/main/java/edu/homework1/NumberOfDigits.java
+++ src/main/java/edu/homework1/NumberOfDigits.java
@@ -3,6 +3,7 @@ package edu.homework1;
 public final class NumberOfDigits {
     private NumberOfDigits() {}
 
+    @SuppressWarnings("MagicNumber")
     public static int countDigits(int inputNumber) {
         int numberOfDigits = 0;
         int currentNumber = inputNumber;
