b436d46d2feed92ba36e6cc729bb7a835cd81680
not-Whale <rezepinn@yandex.ru>
2023-10-12 21:57:06 +0400

    Переписал Expr так, чтобы значение вычислялось по запросу без промежуточных вычислений

--- src/main/java/edu/homework2/Expr.java
+++ src/main/java/edu/homework2/Expr.java
@@ -4,7 +4,6 @@ public sealed interface Expr {
     double evaluate();
 
     public record Constant(double value) implements Expr {
-
         public Constant(Expr exprValue) {
             this(exprValue.evaluate());
         }
@@ -15,110 +14,109 @@ public sealed interface Expr {
         }
     }
 
-    public record Negate(double value) implements Expr {
-
-        public Negate(Expr exprValue) {
-            this(exprValue.evaluate());
+    public record Negate(Expr value) implements Expr {
+        public Negate(double doubleValue) {
+            this(new Constant(doubleValue));
         }
 
         @Override
         public double evaluate() {
-            return -this.value;
+            return (-1) * this.value.evaluate();
         }
     }
 
-    public record Exponent(double base, double exp) implements Expr {
-        public Exponent(double base, Expr exprExp) {
-            this(base, exprExp.evaluate());
+    public record Exponent(Expr base, Expr power) implements Expr {
+        public Exponent(double doubleBase, Expr power) {
+            this(new Constant(doubleBase), power);
         }
 
-        public Exponent(Expr exprBase, double exp) {
-            this(exprBase.evaluate(), exp);
+        public Exponent(Expr base, double doublePower) {
+            this(base, new Constant(doublePower));
         }
 
-        public Exponent(Expr exprBase, Constant constantExp) {
-            this(exprBase.evaluate(), constantExp.evaluate());
+        public Exponent(double doubleBase, double doublePower) {
+            this(new Constant(doubleBase), new Constant(doublePower));
         }
 
         @Override
         public double evaluate() {
-            return Math.pow(this.base, this.exp);
+            return Math.pow(this.base.evaluate(), this.power.evaluate());
         }
     }
 
-    public record Addition(double firstTerm, double secondTerm) implements Expr {
-        public Addition(double firstTerm, Expr exprSecondTerm) {
-            this(firstTerm, exprSecondTerm.evaluate());
+    public record Addition(Expr leftTerm, Expr rightTerm) implements Expr {
+        public Addition(double doubleLeftTerm, Expr rightTerm) {
+            this(new Constant(doubleLeftTerm), rightTerm);
         }
 
-        public Addition(Expr exprFirstTerm, double secondTerm) {
-            this(exprFirstTerm.evaluate(), secondTerm);
+        public Addition(Expr leftTerm, double doubleRightTerm) {
+            this(leftTerm, new Constant(doubleRightTerm));
         }
 
-        public Addition(Expr exprFirstTerm, Expr exprSecondTerm) {
-            this(exprFirstTerm.evaluate(), exprSecondTerm.evaluate());
+        public Addition(double doubleLeftTerm, double doubleRightTerm) {
+            this(new Constant(doubleLeftTerm), new Constant(doubleRightTerm));
         }
 
         @Override
         public double evaluate() {
-            return this.firstTerm + this.secondTerm;
+            return this.leftTerm.evaluate() + this.rightTerm.evaluate();
         }
     }
 
-    public record Subtraction(double firstTerm, double secondTerm) implements Expr {
-        public Subtraction(double firstTerm, Expr exprSecondTerm) {
-            this(firstTerm, exprSecondTerm.evaluate());
+    public record Subtraction(Expr leftTerm, Expr rightTerm) implements Expr {
+        public Subtraction(double doubleLeftTerm, Expr rightTerm) {
+            this(new Constant(doubleLeftTerm), rightTerm);
         }
 
-        public Subtraction(Expr exprFirstTerm, double secondTerm) {
-            this(exprFirstTerm.evaluate(), secondTerm);
+        public Subtraction(Expr leftTerm, double doubleRightTerm) {
+            this(leftTerm, new Constant(doubleRightTerm));
         }
 
-        public Subtraction(Expr exprFirstTerm, Expr exprSecondTerm) {
-            this(exprFirstTerm.evaluate(), exprSecondTerm.evaluate());
+        public Subtraction(double doubleLeftTerm, double doubleRightTerm) {
+            this(new Constant(doubleLeftTerm), new Constant(doubleRightTerm));
         }
 
         @Override
         public double evaluate() {
-            return this.firstTerm - this.secondTerm;
+            return this.leftTerm.evaluate() - this.rightTerm.evaluate();
         }
     }
 
-    public record Multiplication(double firstMul, double secondMul) implements Expr {
-        public Multiplication(double firstMul, Expr exprSecondMul) {
-            this(firstMul, exprSecondMul.evaluate());
+    public record Multiplication(Expr leftMul, Expr rightMul) implements Expr {
+        public Multiplication(double doubleLeftMul, Expr rightMul) {
+            this(new Constant(doubleLeftMul), rightMul);
         }
 
-        public Multiplication(Expr exprFirstMul, double secondMul) {
-            this(exprFirstMul.evaluate(), secondMul);
+        public Multiplication(Expr leftMul, double doubleRightMul) {
+            this(leftMul, new Constant(doubleRightMul));
         }
 
-        public Multiplication(Expr exprFirstMul, Expr exprSecondMul) {
-            this(exprFirstMul.evaluate(), exprSecondMul.evaluate());
+        public Multiplication(double doubleLeftMul, double doubleRightMul) {
+            this(new Constant(doubleLeftMul), new Constant(doubleRightMul));
         }
 
         @Override
         public double evaluate() {
-            return this.firstMul * this.secondMul;
+            return this.leftMul.evaluate() * this.rightMul.evaluate();
         }
     }
 
-    public record Division(double firstMul, double secondMul) implements Expr {
-        public Division(double firstMul, Expr exprSecondMul) {
-            this(firstMul, exprSecondMul.evaluate());
+    public record Division(Expr leftMul, Expr rightMul) implements Expr {
+        public Division(double doubleLeftMul, Expr rightMul) {
+            this(new Constant(doubleLeftMul), rightMul);
         }
 
-        public Division(Expr exprFirstMul, double secondMul) {
-            this(exprFirstMul.evaluate(), secondMul);
+        public Division(Expr leftMul, double doubleRightMul) {
+            this(leftMul, new Constant(doubleRightMul));
         }
 
-        public Division(Expr exprFirstMul, Expr exprSecondMul) {
-            this(exprFirstMul.evaluate(), exprSecondMul.evaluate());
+        public Division(double doubleLeftMul, double doubleRightMul) {
+            this(new Constant(doubleLeftMul), new Constant(doubleRightMul));
         }
 
         @Override
         public double evaluate() {
-            return this.firstMul / this.secondMul;
+            return this.leftMul.evaluate() / this.rightMul.evaluate();
         }
     }
 }
