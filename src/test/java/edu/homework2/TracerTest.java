5ef4beb275fdaa92bdf1593e42c07b526c9c61fb
not-Whale <rezepinn@yandex.ru>
2023-10-13 10:46:15 +0400

    Добавил тесты для 4 задачи

---
+++ src/test/java/edu/homework2/TracerTest.java
@@ -0,0 +1,54 @@
+package edu.homework2;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TracerTest {
+    @Test
+    void callingInfoTest() {
+        // given
+        // --
+
+        // when
+        CallingInfo callingInfo = Tracer.getCallingInfo();
+
+        // then
+        assertThat(callingInfo.className()).isEqualTo("edu.homework2.TracerTest");
+        assertThat(callingInfo.methodName()).isEqualTo("callingInfoTest");
+    }
+
+    @Test
+    void callingInfoStaticMethod() {
+        // given
+        class StaticMethodCaller {
+            public static CallingInfo staticMethod() {
+                return Tracer.getCallingInfo();
+            }
+        }
+
+        // when
+        CallingInfo callingInfo = StaticMethodCaller.staticMethod();
+
+        // then
+        assertThat(callingInfo.className()).isEqualTo("edu.homework2.TracerTest$1StaticMethodCaller");
+        assertThat(callingInfo.methodName()).isEqualTo("staticMethod");
+    }
+
+    @Test
+    void callingInfoNonStaticMethod() {
+        // given
+        class NonStaticMethodCaller {
+            public CallingInfo nonStaticMethod() {
+                return Tracer.getCallingInfo();
+            }
+        }
+
+        // when
+        NonStaticMethodCaller nonStaticMethodCaller = new NonStaticMethodCaller();
+        CallingInfo callingInfo = nonStaticMethodCaller.nonStaticMethod();
+
+        // then
+        assertThat(callingInfo.className()).isEqualTo("edu.homework2.TracerTest$1NonStaticMethodCaller");
+        assertThat(callingInfo.methodName()).isEqualTo("nonStaticMethod");
+    }
+}
