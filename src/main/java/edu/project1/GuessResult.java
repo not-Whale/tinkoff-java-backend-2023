599470198eed53bd6624d6447ae446c79988a401
not-Whale <rezepinn@yandex.ru>
2023-10-13 11:39:36 +0400

    Добавил набросок Виселицы

---
+++ src/main/java/edu/project1/GuessResult.java
@@ -0,0 +1,21 @@
+package edu.project1;
+
+sealed interface GuessResult {
+    char[] state();
+
+    int attempt();
+
+    int maxAttempts();
+
+    String message();
+
+    record Defeat(char[] state, int attempt, int maxAttempts, String message) implements GuessResult {}
+
+    record Win(char[] state, int attempt, int maxAttempts, String message) implements GuessResult {}
+
+    record SuccessfulGuess(char[] state, int attempt, int maxAttempts, String message) implements GuessResult {}
+
+    record FailedGuess(char[] state, int attempt, int maxAttempts, String message) implements GuessResult {}
+
+    record RepeatedGuess(char[] state, int attempt, int maxAttempts, String message) implements GuessResult {}
+}
