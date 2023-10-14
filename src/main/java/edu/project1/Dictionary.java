5f6d84d5cb45b6eb5bd5d18f8afa20626b9643f1
not-Whale <rezepinn@yandex.ru>
2023-10-14 13:32:47 +0400

    Реализовал вытягивание случайного слова через API сервиса для генерации таковых

---
+++ src/main/java/edu/project1/Dictionary.java
@@ -0,0 +1,40 @@
+package edu.project1;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+
+class Dictionary {
+    private static final String REQUEST_URL = "http://free-generator.ru/generator.php?action=word&type=1";
+
+    private static final String KEY = "word";
+
+    private Dictionary() {}
+
+    public static String randomWord() throws IOException, InterruptedException {
+        return parseRandomWordFromJSON(getJSON());
+    }
+
+    private static String getJSON() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(REQUEST_URL))
+            .GET()
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    private static String parseRandomWordFromJSON(String jsonStr) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Map<String, String>> jsonToMap = objectMapper.readValue(jsonStr, new TypeReference<>() {});
+        return jsonToMap.get(KEY).get(KEY).toLowerCase();
+    }
+}
