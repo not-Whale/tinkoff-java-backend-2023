4f9c6d9cbc3e01b30236264a922fbc8648502587
not-Whale <rezepinn@yandex.ru>
2023-10-16 23:05:59 +0400

    Записал объявления в одну строчку

--- src/main/java/edu/project1/GuessResult.java
+++ src/main/java/edu/project1/GuessResult.java
@@ -3,18 +3,15 @@ package edu.project1;
 sealed interface GuessResult {
     char[] state();
 
-    record Defeat(char[] state)
-        implements GuessResult {}
+    record Defeat(char[] state) implements GuessResult {}
 
-    record Win(char[] state)
-        implements GuessResult {}
+    record Win(char[] state) implements GuessResult {}
 
-    record SuccessfulGuess(char[] state, char symbol, int symbolsOpened)
-        implements GuessResult {}
+    record SuccessfulGuess(char[] state, char symbol, int symbolsOpened) implements GuessResult {}
 
-    record FailedGuess(char[] state, int attempts, int maxAttempts)
-        implements GuessResult {}
+    record FailedGuess(char[] state, int attempts, int maxAttempts) implements GuessResult {}
 
-    record RepeatedGuess(char[] state, char[] attempts)
-        implements GuessResult {}
+    record RepeatedGuess(char[] state, char[] attempts) implements GuessResult {}
+
+    record GiveUp(char[] state) implements GuessResult {}
 }
