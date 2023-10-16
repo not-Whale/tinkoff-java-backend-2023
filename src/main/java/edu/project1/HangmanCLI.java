35a4186d1eab1fba9e0297a144df0270af874085
not-Whale <rezepinn@yandex.ru>
2023-10-16 23:15:14 +0400

    Убрал тестирующий public static void main, изменил названия констант на подходящие

--- src/main/java/edu/project1/HangmanCLI.java
+++ src/main/java/edu/project1/HangmanCLI.java
@@ -14,25 +14,25 @@ public class HangmanCLI {
 
     private static final char BAD_INPUT_CHAR = '*';
 
-    private static final String alphabetRegex = "[а-яА-Я]";
+    private static final String ALPHABET_REGEX = "[а-яА-Я]";
 
-    private static final String badInputMessage = "Некорректный ввод: введите символ кириллического алфавита!";
+    private static final String BAD_INPUT_MESSAGE = "Некорректный ввод: введите символ кириллического алфавита!";
 
-    private static final String winMessage = "Вы победили!";
+    private static final String WIN_MESSAGE = "Вы победили!";
 
-    private static final String defeatMessage = "Вы проиграли!";
+    private static final String DEFEAT_MESSAGE = "Вы проиграли!";
 
-    private static final String giveUpMessage = "Вы сдались!";
+    private static final String GIVE_UP_MESSAGE = "Вы сдались!";
 
-    private static final String somethingWrongMessage = "Упс... Что-то пошло не так!";
+    private static final String SOMETHING_WRONG_MESSAGE = "Упс... Что-то пошло не так!";
 
-    private static final String guessSymbolMessage = "Угадайте букву: ";
+    private static final String GUESS_SYMBOL_MESSAGE = "Угадайте букву: ";
 
-    private static final String endGame = "Спасибо за игру!";
+    private static final String END_GAME = "Спасибо за игру!";
 
-    private HangmanCLI() {}
+    public HangmanCLI() {}
 
-    public static void main(String[] args) {
+    public void run() {
         String answer = Dictionary.randomWord();
         Session session = new Session(answer, ATTEMPTS);
         Scanner scanner = new Scanner(System.in);
@@ -44,7 +44,7 @@ public class HangmanCLI {
             try {
                 char userGuess = readUserGuess(scanner);
                 if (userGuess == BAD_INPUT_CHAR) {
-                    LOGGER.info(badInputMessage);
+                    LOGGER.info(BAD_INPUT_MESSAGE);
                     continue;
                 }
 
@@ -60,32 +60,32 @@ public class HangmanCLI {
                 isGameOver = true;
             }
         }
-        LOGGER.info(endGame);
+        LOGGER.info(END_GAME);
     }
 
-    private static char readUserGuess(Scanner scanner) {
-        LOGGER.info(guessSymbolMessage);
+    private char readUserGuess(Scanner scanner) {
+        LOGGER.info(GUESS_SYMBOL_MESSAGE);
         String userInput = scanner.nextLine().strip().toLowerCase();
-        if (userInput.length() != 1 || !Pattern.matches(alphabetRegex, userInput)) {
+        if (userInput.length() != 1 || !Pattern.matches(ALPHABET_REGEX, userInput)) {
             return BAD_INPUT_CHAR;
         }
         return userInput.charAt(0);
     }
 
-    private static int checkAndPrintState(GuessResult guessResult) {
+    private int checkAndPrintState(GuessResult guessResult) {
         int returnCode = 0;
         switch (guessResult) {
             case GuessResult.Win win -> {
-                LOGGER.info(winMessage);
+                LOGGER.info(WIN_MESSAGE);
                 returnCode = 1;
             }
             case GuessResult.Defeat defeat -> {
-                LOGGER.info(defeatMessage);
+                LOGGER.info(DEFEAT_MESSAGE);
                 printAnswerState(defeat);
                 returnCode = 2;
             }
             case GuessResult.GiveUp giveUp -> {
-                LOGGER.info(giveUpMessage);
+                LOGGER.info(GIVE_UP_MESSAGE);
                 printAnswerState(giveUp);
                 returnCode = 2;
             }
@@ -102,27 +102,27 @@ public class HangmanCLI {
                 printAnswerState(failedGuess);
             }
             default -> {
-                LOGGER.info(somethingWrongMessage);
+                LOGGER.info(SOMETHING_WRONG_MESSAGE);
                 returnCode = -1;
             }
         }
         return returnCode;
     }
 
-    private static String getRepeatedGuessMessage(GuessResult.RepeatedGuess repeatedGuess) {
+    private String getRepeatedGuessMessage(GuessResult.RepeatedGuess repeatedGuess) {
         return "Вы уже открывали данный символ! Ваши попытки: " + Arrays.toString(repeatedGuess.attempts()) + ".";
     }
 
-    private static String getFailedGuessMessage(GuessResult.FailedGuess failedGuess) {
+    private String getFailedGuessMessage(GuessResult.FailedGuess failedGuess) {
         int attemptsLeft = failedGuess.maxAttempts() - failedGuess.attempts();
         return "Неверно! Доступное количество попыток: " + attemptsLeft + " из " + failedGuess.maxAttempts() + ".";
     }
 
-    private static String getSuccessfulGuessMessage(GuessResult.SuccessfulGuess successfulGuess) {
+    private String getSuccessfulGuessMessage(GuessResult.SuccessfulGuess successfulGuess) {
         return "Символ \"" + successfulGuess.symbol() + "\" открыт " + successfulGuess.symbolsOpened() + " раз(-а)!";
     }
 
-    private static void printAnswerState(GuessResult guessResult) {
+    private void printAnswerState(GuessResult guessResult) {
         LOGGER.info("Слово: " + String.valueOf(guessResult.state()) + ".");
     }
 }
