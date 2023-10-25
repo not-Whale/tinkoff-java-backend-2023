9141ddc3de18f78df8be60c4b1c41839eebe833d
not-Whale <rezepinn@yandex.ru>
2023-10-25 18:44:03 +0400

    Добавил решение третьей задачи

---
+++ src/main/java/edu/homework3/FreqDict.java
@@ -0,0 +1,30 @@
+package edu.homework3;
+
+import java.util.HashMap;
+
+public class FreqDict<T> {
+    private final HashMap<T, Integer> frequencyDictionary = new HashMap<>();
+
+    public FreqDict(T[] inputSeq) throws IllegalArgumentException {
+        if (inputSeq == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (T elem : inputSeq) {
+            addElem(elem);
+        }
+    }
+
+    public HashMap<T, Integer> getDict() {
+        return frequencyDictionary;
+    }
+
+    private void addElem(T elem) {
+        if (frequencyDictionary.containsKey(elem)) {
+            int freq = frequencyDictionary.get(elem);
+            frequencyDictionary.replace(elem, freq, freq + 1);
+        } else {
+            frequencyDictionary.put(elem, 1);
+        }
+    }
+}
---
+++ src/test/java/edu/homework3/FreqDictTest.java
@@ -0,0 +1,75 @@
+package edu.homework3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("Частотный словарь.")
+public class FreqDictTest {
+    @Test
+    @DisplayName("Составление частотного словаря строк.")
+    void freqDictFromString() {
+        // given
+        String[] inputSeq = new String[] {"this", "and", "that", "and"};
+        HashMap<String, Integer> answer = new HashMap<>() {{
+            put("this", 1); put("and", 2); put("that", 1);
+        }};
+
+        // when
+        FreqDict<String> freqDict = new FreqDict<>(inputSeq);
+        HashMap<String, Integer> map = freqDict.getDict();
+
+        // then
+        assertThat(map).isNotNull().isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("Составление частного словаря численного типа.")
+    void freqDictFromNumeric() {
+        // given
+        Integer[] inputSeq = new Integer[] {1, 1, 2, 2};
+        HashMap<Integer, Integer> answer = new HashMap<>() {{
+            put(1, 2); put(2, 2);
+        }};
+
+        // when
+        FreqDict<Integer> freqDict = new FreqDict<>(inputSeq);
+        HashMap<Integer, Integer> map = freqDict.getDict();
+
+        // then
+        assertThat(map).isNotNull().isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("Возврат пустого словаря на пустой вход.")
+    void freqDictEmptyInput() {
+        // given
+        Character[] inputSeq = new Character[0];
+        HashMap<Character, Integer> answer = new HashMap<>();
+
+        // when
+        FreqDict<Character> freqDict = new FreqDict<>(inputSeq);
+        HashMap<Character, Integer> map = freqDict.getDict();
+
+        // then
+        assertThat(map).isNotNull().isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("Null-аргумент.")
+    void freqDictNullInput() {
+        // given
+        Byte[] inputSeq = null;
+
+        // when
+        Exception exception = assertThrows(
+            IllegalArgumentException.class,
+            () -> new FreqDict<Byte>(inputSeq)
+        );
+
+        // then
+        assertThat(exception).isNotNull().isInstanceOf(IllegalArgumentException.class);
+    }
+}
