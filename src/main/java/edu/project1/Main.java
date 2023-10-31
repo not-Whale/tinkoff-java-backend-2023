1186ebe88dbd7a78b7e91e6b04146c64d631a12d
not-Whale <rezepinn@yandex.ru>
2023-10-31 16:38:32 +0400

    Добавил конструктор утилитного класса запуска

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -1,6 +1,8 @@
 package edu.project1;
 
 public class Main {
+    private Main() {}
+
     public static void main(String[] args) {
         HangmanCLI hangman = new HangmanCLI();
         hangman.run();
