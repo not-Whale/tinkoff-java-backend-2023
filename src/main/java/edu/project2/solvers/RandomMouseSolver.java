c73d51cce08a3d770feee782883b5460202863b8
not-Whale <rezepinn@yandex.ru>
2023-11-04 13:19:40 +0400

    Заменил вызов дублирующей функции на аналогичную из утилит

--- src/main/java/edu/project2/solvers/RandomMouseSolver.java
+++ src/main/java/edu/project2/solvers/RandomMouseSolver.java
@@ -3,6 +3,7 @@ package edu.project2.solvers;
 import edu.project2.maze.Cell;
 import edu.project2.maze.Coordinate;
 import edu.project2.maze.Maze;
+import edu.project2.utils.MazeUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -24,6 +25,7 @@ public class RandomMouseSolver implements Solver {
 
         List<Coordinate> path = new ArrayList<>();
         Random random = new Random();
+        Cell[] passages = maze.getPassages();
 
         Cell startCell = maze.getCell(start.row() * 2 + 1, start.col() * 2 + 1);
         Cell endCell = maze.getCell(end.row() * 2 + 1, end.col() * 2 + 1);
@@ -31,45 +33,11 @@ public class RandomMouseSolver implements Solver {
         Cell currentCell = startCell;
         path.add(new Coordinate(startCell.row(), startCell.col()));
         while (!currentCell.equals(endCell)) {
-            Cell[] moves = getNearbyMoves(maze, currentCell);
+            Cell[] moves = MazeUtils.getMovesWithWhitelist(maze, currentCell, 1, List.of(passages));
             currentCell = moves[random.nextInt(moves.length)];
             path.add(new Coordinate(currentCell.row(), currentCell.col()));
         }
 
         return path;
-    }
-
-    private Cell[] getNearbyMoves(Maze maze, Cell cell) {
-        ArrayList<Cell> nearbyCells = new ArrayList<>();
-
-        if (cell.row() > 0) {
-            Cell bottomCell = maze.getCell(cell.row() - 1, cell.col());
-            if (bottomCell.type().equals(Cell.Type.PASSAGE)) {
-                nearbyCells.add(bottomCell);
-            }
-        }
-
-        if (cell.row() < maze.getHeight() - 1) {
-            Cell topCell = maze.getCell(cell.row() + 1, cell.col());
-            if (topCell.type().equals(Cell.Type.PASSAGE)) {
-                nearbyCells.add(topCell);
-            }
-        }
-
-        if (cell.col() > 0) {
-            Cell rightCell = maze.getCell(cell.row(), cell.col() - 1);
-            if (rightCell.type().equals(Cell.Type.PASSAGE)) {
-                nearbyCells.add(rightCell);
-            }
-        }
-
-        if (cell.col() < maze.getWidth() - 1) {
-            Cell leftCell = maze.getCell(cell.row(), cell.col() + 1);
-            if (leftCell.type().equals(Cell.Type.PASSAGE)) {
-                nearbyCells.add(leftCell);
-            }
-        }
-
-        return nearbyCells.toArray(new Cell[] {});
     }
 }
