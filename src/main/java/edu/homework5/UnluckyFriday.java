115e248e9d92eb05bf048bffe2bf074c8b0fa14e
not-Whale <rezepinn@yandex.ru>
2023-11-09 12:13:06 +0400

    Выделил константы

--- src/main/java/edu/homework5/UnluckyFriday.java
+++ src/main/java/edu/homework5/UnluckyFriday.java
@@ -14,6 +14,11 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class UnluckyFriday {
+
+    public static final int UNLUCKY_DAY_OF_MONTH = 13;
+
+    public static final int DAYS_IN_WEEK = 7;
+
     private UnluckyFriday() {
     }
 
@@ -23,46 +28,40 @@ public class UnluckyFriday {
         }
 
         String datePattern = "yyyy-MM-dd";
-        SimpleDateFormat sdf = new SimpleDateFormat(datePattern);
-
         String startDateString = year + "-01-01";
         String endDateString = year + "-12-31";
-
         ArrayList<Date> unluckyFridays = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat(datePattern);
 
         try {
             GregorianCalendar calendar = new GregorianCalendar();
             Date startDate = sdf.parse(startDateString);
             Date endDate = sdf.parse(endDateString);
             calendar.setTime(startDate);
-
             boolean reachedFriday = false;
             while (calendar.getTime().before(endDate)) {
                 if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) {
                     reachedFriday = true;
-                    if (calendar.get(Calendar.DAY_OF_MONTH) == 13) {
+                    if (calendar.get(Calendar.DAY_OF_MONTH) == UNLUCKY_DAY_OF_MONTH) {
                         unluckyFridays.add(calendar.getTime());
                     }
                 }
-
                 if (reachedFriday) {
-                    calendar.add(Calendar.DAY_OF_YEAR, 7);
+                    calendar.add(Calendar.DAY_OF_YEAR, DAYS_IN_WEEK);
                 } else {
                     calendar.add(Calendar.DAY_OF_YEAR, 1);
                 }
             }
-
         } catch (ParseException e) {
             throw new IllegalArgumentException("Формат даты: \"yyyy-MM-dd\"");
         }
-
         return unluckyFridays.toArray(new Date[] {});
     }
 
     public static LocalDate getNextUnluckyFriday(LocalDate currentDate) {
         return currentDate.with((temporal -> {
             Temporal nextFriday = temporal.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
-            while (nextFriday.get(ChronoField.DAY_OF_MONTH) != 13) {
+            while (nextFriday.get(ChronoField.DAY_OF_MONTH) != UNLUCKY_DAY_OF_MONTH) {
                 nextFriday = nextFriday.plus(1, ChronoUnit.WEEKS);
             }
             return nextFriday;
