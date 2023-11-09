918babc1671cb3c44042f8c84c1b289f34f58330
not-Whale <rezepinn@yandex.ru>
2023-11-09 14:32:12 +0400

    Привел регулярные выражения к однообразному виду

--- src/main/java/edu/homework5/date_parser/DateUtils.java
+++ src/main/java/edu/homework5/date_parser/DateUtils.java
@@ -13,7 +13,7 @@ public class DateUtils {
     public static Optional<LocalDate> parseDate(String date) {
         return Stream.of(
             new FormatParser("yyyy-MM-dd"),
-            new FormatParser("yyyy-MM-d", "^(\\d{1,4})-(\\d{2})-([1-9]|[1-3][0-9])$"),
+            new FormatParser("yyyy-MM-d", "^([0-9]{4})-([0-9]{2})-([1-9]|[1-3][0-9])$"),
             new FormatParser("d/M/yyyy", "^([1-9]|[1-3][0-9])/([1-9]|1[0-2])/([0-9]{4})$"),
             new FormatParser("d/M/yy", "^([1-9]|[1-3][0-9])/([1-9]|1[0-2])/([0-9]{2})$"),
             new VerbalParser(),
