41d34cbe725f906797f0aff7a32bf07216289222
not-Whale <rezepinn@yandex.ru>
2023-11-09 12:08:31 +0400

    Выделил отдельные методы для операций, изменил название переменной с форматом даты

--- src/main/java/edu/homework5/ComputerClubAnalytics.java
+++ src/main/java/edu/homework5/ComputerClubAnalytics.java
@@ -13,30 +13,31 @@ public class ComputerClubAnalytics {
         if (sessions == null) {
             throw new IllegalArgumentException("Список сессий не может быть null!");
         }
-
-        String dttm = "yyyy-MM-dd, hh:mm";
-        SimpleDateFormat sdf = new SimpleDateFormat(dttm);
-
         Double averageSessionDuration = Arrays.stream(sessions)
-            .map(sessionString -> {
-                String[] startEndDate = sessionString.split(" - ");
-                if (startEndDate.length != 2) {
-                    throw new IllegalArgumentException("Формат сессии: \"yyyy-MM-dd, hh:mm - yyyy-MM-dd, hh:mm\"!");
-                }
-                return startEndDate;
-            })
-            .map(startEndDate -> {
-                try {
-                    return Duration.between(
-                        sdf.parse(startEndDate[0]).toInstant(),
-                        sdf.parse(startEndDate[1]).toInstant()
-                    );
-                } catch (ParseException e) {
-                    throw new IllegalArgumentException("Формат даты: \"yyyy-MM-dd, hh:mm\"", e);
-                }
-            })
+            .map(ComputerClubAnalytics::sessionStringToStartEndDate)
+            .map(ComputerClubAnalytics::startEndDateToDuration)
             .collect(Collectors.averagingLong(Duration::toMillis));
-
         return Duration.ofMillis(averageSessionDuration.longValue());
+    }
+
+    private static String[] sessionStringToStartEndDate(String sessionString) {
+        String[] startEndDate = sessionString.split(" - ");
+        if (startEndDate.length != 2) {
+            throw new IllegalArgumentException("Формат сессии: \"yyyy-MM-dd, hh:mm - yyyy-MM-dd, hh:mm\"!");
+        }
+        return startEndDate;
+    }
+
+    private static Duration startEndDateToDuration(String[] startEndDate) throws IllegalArgumentException {
+        String dtmFormat = "yyyy-MM-dd, hh:mm";
+        SimpleDateFormat sdf = new SimpleDateFormat(dtmFormat);
+        try {
+            return Duration.between(
+                sdf.parse(startEndDate[0]).toInstant(),
+                sdf.parse(startEndDate[1]).toInstant()
+            );
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Формат даты: \"yyyy-MM-dd, hh:mm\"", e);
+        }
     }
 }
