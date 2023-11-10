23df798dda15ef5f445e95f8fa88fb3a0db31264
not-Whale <rezepinn@yandex.ru>
2023-11-10 11:48:55 +0400

    Сделал два варианта: с только одним и хотя бы одним символом из множества

--- src/main/java/edu/homework5/PasswordValidator.java
+++ src/main/java/edu/homework5/PasswordValidator.java
@@ -3,8 +3,13 @@ package edu.homework5;
 public class PasswordValidator {
     private PasswordValidator() {}
 
-    public static boolean validate(String password) {
+    public static boolean hasOnlyOneSymbol(String password) {
         String regex = "^(\\w)*[~!@#$%^&*|](\\w)*$";
+        return password.matches(regex);
+    }
+
+    public static boolean hasAtLeastOneSymbol(String password) {
+        String regex = "((\\w)*[~!@#$%^&*|](\\w)*)*";
         return password.matches(regex);
     }
 }
