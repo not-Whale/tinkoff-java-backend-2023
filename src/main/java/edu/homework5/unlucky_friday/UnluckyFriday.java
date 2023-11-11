a6dbad1d2d184f5aa4e853aa1068731a97eee3b8
not-Whale <rezepinn@yandex.ru>
2023-11-11 20:34:05 +0400

    Переписал поиск пятницы 13-го под новый формат даты

--- src/main/java/edu/homework5/unlucky_friday/UnluckyFriday.java
+++ src/main/java/edu/homework5/unlucky_friday/UnluckyFriday.java
@@ -1,61 +1,47 @@
 package edu.homework5.unlucky_friday;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.List;
 
 public class UnluckyFriday {
 
     public static final int UNLUCKY_DAY_OF_MONTH = 13;
-
-    public static final int DAYS_IN_WEEK = 7;
+    public static final int LAST_DAY_OF_DECEMBER = 31;
+    public static final int FIRST_DAY_OF_JANUARY = 1;
+    public static final int FRIDAY = 5;
 
     private UnluckyFriday() {
     }
 
-    public static Date[] findUnluckyFridaysByYear(int year) throws IllegalArgumentException {
+    public static LocalDate[] findUnluckyFridaysByYear(int year) throws IllegalArgumentException {
         if (year < 1) {
             throw new IllegalArgumentException("Год должен быть положительным числом!");
         }
-
-        String datePattern = "yyyy-MM-dd";
-        String startDateString = year + "-01-01";
-        String endDateString = year + "-12-31";
-        ArrayList<Date> unluckyFridays = new ArrayList<>();
-        SimpleDateFormat sdf = new SimpleDateFormat(datePattern);
-
-        try {
-            GregorianCalendar calendar = new GregorianCalendar();
-            Date startDate = sdf.parse(startDateString);
-            Date endDate = sdf.parse(endDateString);
-            calendar.setTime(startDate);
-            boolean reachedFriday = false;
-            while (calendar.getTime().before(endDate)) {
-                if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) {
-                    reachedFriday = true;
-                    if (calendar.get(Calendar.DAY_OF_MONTH) == UNLUCKY_DAY_OF_MONTH) {
-                        unluckyFridays.add(calendar.getTime());
-                    }
-                }
-                if (reachedFriday) {
-                    calendar.add(Calendar.DAY_OF_YEAR, DAYS_IN_WEEK);
-                } else {
-                    calendar.add(Calendar.DAY_OF_YEAR, 1);
+        List<LocalDate> unluckyFridays = new ArrayList<>();
+        boolean reachedFriday = false;
+        LocalDate endDate = LocalDate.of(year, Month.DECEMBER, LAST_DAY_OF_DECEMBER);
+        LocalDate currentDate = LocalDate.of(year, Month.JANUARY, FIRST_DAY_OF_JANUARY);
+        while (currentDate.isBefore(endDate)) {
+            if (currentDate.get(ChronoField.DAY_OF_WEEK) == FRIDAY) {
+                reachedFriday = true;
+                if (currentDate.getDayOfMonth() == UNLUCKY_DAY_OF_MONTH) {
+                    unluckyFridays.add(currentDate);
                 }
             }
-        } catch (ParseException e) {
-            throw new IllegalArgumentException("Формат даты: \"yyyy-MM-dd\"");
+            if (reachedFriday) {
+                currentDate = currentDate.plusWeeks(1);
+            } else {
+                currentDate = currentDate.plusDays(1);
+            }
         }
-        return unluckyFridays.toArray(new Date[] {});
+        return unluckyFridays.toArray(LocalDate[]::new);
     }
 
     public static LocalDate getNextUnluckyFriday(LocalDate currentDate) throws IllegalArgumentException {
