5ef2a097c2562680c1465e05051784569ab6357f
not-Whale <rezepinn@yandex.ru>
2023-11-20 01:19:03 +0400

    Добавил форматирование для консоли

--- src/main/java/edu/project3/formatters/FormatUtils.java
+++ src/main/java/edu/project3/formatters/FormatUtils.java
@@ -5,6 +5,84 @@ import java.util.Arrays;
 public class FormatUtils {
     private FormatUtils() {}
 
+    public static String reportToString(String[] names, String[] values, String statisticName, String valuesName) {
+        if (names.length != values.length) {
+            throw new IllegalArgumentException();
+        }
+
+        long tableLength = names.length;
+        long lastLine = tableLength - 1;
+
+        int maxNameLength = Arrays.stream(names)
+            .map(String::length)
+            .max((a, b) -> b - a)
+            .orElse(0);
+        maxNameLength = Math.max(maxNameLength, statisticName.length());
+
+        int maxValuesLength = Arrays.stream(values)
+            .map(String::length)
+            .max((a, b) -> b - a)
+            .orElse(0);
+        maxValuesLength = Math.max(maxValuesLength, valuesName.length());
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+            .append("┌")
+            .repeat("─", maxNameLength)
+            .append("┬")
+            .repeat("─", maxValuesLength)
+            .append("┐")
+            .append("\n");
+
+        stringBuilder
+            .append("│")
+            .append(statisticName)
+            .repeat(" ", maxNameLength - statisticName.length())
+            .append("│")
+            .append(valuesName)
+            .repeat(" ", maxValuesLength - valuesName.length())
+            .append("│")
+            .append("\n");
+
+        stringBuilder
+            .append("├")
+            .repeat("─", maxNameLength)
+            .append("┼")
+            .repeat("─", maxValuesLength)
+            .append("┤")
+            .append("\n");
+
+        for (int i = 0; i < tableLength; i++) {
+            stringBuilder
+                .append("|")
+                .append(names[i])
+                .repeat(" ", maxNameLength - names[i].length())
+                .append("│")
+                .append(values[i])
+                .repeat(" ", maxValuesLength - values[i].length())
+                .append("│")
+                .append("\n");
+            if (i != lastLine) {
+                stringBuilder
+                    .append("├")
+                    .repeat("─", maxNameLength)
+                    .append("┼")
+                    .repeat("─", maxValuesLength)
+                    .append("┤")
+                    .append("\n");
+            } else {
+                stringBuilder
+                    .append("└")
+                    .repeat("─", maxNameLength)
+                    .append("┴")
+                    .repeat("─", maxValuesLength)
+                    .append("┘")
+                    .append("\n");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
     public static String reportToAdoc(String[] names, String[] values, String statisticName, String valuesName) {
         if (names.length != values.length) {
             throw new IllegalArgumentException();
