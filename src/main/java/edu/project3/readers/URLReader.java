3fdda7ab7b12bb94c50173ba99d418e61ea17363
not-Whale <rezepinn@yandex.ru>
2023-11-19 22:01:30 +0400

    Добавил чтение по URL

---
+++ src/main/java/edu/project3/readers/URLReader.java
@@ -0,0 +1,50 @@
+package edu.project3.readers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class URLReader implements Reader {
+    private static final String PROTOCOL_DOMAIN_PATTERN =
+        "((http|https)://(www.)?)?[a-zA-Z0-9]{2,}(\\.[a-zA-Z0-9]{2,})*";
+
+    private static final String SUBDOMAIN_PATTERN =
+        "(/[a-z-A-Z0-9%_-]{2,})*";
+
+    private static final String FILE_PATTERN =
+        "(/[a-zA-Z0-9%_-]{2,}\\.[a-zA-Z0-9]{2,})?";
+
+    private static final String URL_PATTERN =
+        "^" + PROTOCOL_DOMAIN_PATTERN + SUBDOMAIN_PATTERN + FILE_PATTERN + "$";
+
+    private String path;
+
+    public URLReader(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public boolean canRead() {
+        return path.matches(URL_PATTERN);
+    }
+
+    @Override
+    public String[] readLogs() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(path))
+            .GET()
+            .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String responseBody = response.body();
+            return responseBody.split(" ");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            return new String[0];
+        }
+    }
+}
