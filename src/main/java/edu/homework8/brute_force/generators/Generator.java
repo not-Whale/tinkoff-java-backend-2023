8ac01fc5925e4a8e14b7a748ae71659c32a66970
not-Whale <rezepinn@yandex.ru>
2023-12-04 00:22:46 +0400

    Добавил генерацию по алфавиту

--- src/main/java/edu/homework8/brute_force/generators/Generator.java
+++ src/main/java/edu/homework8/brute_force/generators/Generator.java
@@ -3,20 +3,17 @@ package edu.homework8.brute_force.generators;
 import java.util.List;
 
 public class Generator {
-    private final List<String> alphabet = List.of(
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "K",
-        "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V",
-        "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6",
-        "7", "8", "9"
-    );
+    private final List<String> alphabet;
 
-    private final int alphabetSize = alphabet.size();
+    private final int alphabetSize;
 
     private final int passwordSize;
 
     private final int[] positions;
 
-    public Generator(int passwordSize) {
+    public Generator(List<String> alphabet, int passwordSize) {
+        this.alphabet = alphabet;
+        this.alphabetSize = alphabet.size();
         this.passwordSize = passwordSize;
         positions = new int[passwordSize];
     }
