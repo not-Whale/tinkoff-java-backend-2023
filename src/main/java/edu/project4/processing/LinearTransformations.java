b0d92d34b760759c6b51184828969af5f5fbdbdf
not-Whale <rezepinn@yandex.ru>
2023-12-10 01:48:01 +0400

    Добавил модификаторы static

--- src/main/java/edu/project4/processing/LinearTransformations.java
+++ src/main/java/edu/project4/processing/LinearTransformations.java
@@ -5,7 +5,7 @@ import edu.project4.coordinates.Point;
 public class LinearTransformations {
     private LinearTransformations() {}
 
-    public Transformation create(double a, double b, double c, double d, double e, double f) {
+    public static Transformation create(double a, double b, double c, double d, double e, double f) {
         return (Point p) -> {
             double newX = a * p.x() + b * p.y() + c;
             double newY = d * p.x() + e * p.y() + f;
