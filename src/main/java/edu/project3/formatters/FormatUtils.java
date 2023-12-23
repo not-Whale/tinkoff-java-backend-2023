3976e1f68a5af72914477d88d16962589a09257b
not-Whale <rezepinn@yandex.ru>
2023-12-24 01:42:41 +0400

    Выделил отдельные методы под рисование результата аналитики строкой

--- src/main/java/edu/project3/formatters/FormatUtils.java
+++ src/main/java/edu/project3/formatters/FormatUtils.java
@@ -4,83 +4,30 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class FormatUtils {
-    private FormatUtils() {}
+    private FormatUtils() {
+    }
 
     public static String reportToString(String[] names, String[] values, String statisticName, String valuesName) {
         if (names.length != values.length) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Column's names must match values!");
         }
 
         long tableLength = names.length;
         long lastLine = tableLength - 1;
-
-        int maxNameLength = Arrays.stream(names)
-            .map(String::length)
-            .max(Comparator.comparingInt(a -> a))
-            .orElse(0);
-        maxNameLength = Math.max(maxNameLength, statisticName.length());
-
-        int maxValuesLength = Arrays.stream(values)
-            .map(String::length)
-            .max(Comparator.comparingInt(a -> a))
-            .orElse(0);
-        maxValuesLength = Math.max(maxValuesLength, valuesName.length());
+        int maxNameLength = getMaxNameLength(names, statisticName);
+        int maxValuesLength = getMaxValuesLength(values, valuesName);
 
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder
-            .append("┌")
-            .repeat("─", maxNameLength)
-            .append("┬")
-            .repeat("─", maxValuesLength)
-            .append("┐")
-            .append("\n");
-
-        stringBuilder
-            .append("│")
-            .append(statisticName)
-            .repeat(" ", maxNameLength - statisticName.length())
-            .append("│")
-            .append(valuesName)
-            .repeat(" ", maxValuesLength - valuesName.length())
-            .append("│")
-            .append("\n");
-
-        stringBuilder
-            .append("├")
-            .repeat("─", maxNameLength)
-            .append("┼")
-            .repeat("─", maxValuesLength)
-            .append("┤")
-            .append("\n");
-
+        appendStringFormattedFirstLine(stringBuilder, maxNameLength, maxValuesLength);
+        appendStringFormattedTitle(stringBuilder, maxNameLength, maxValuesLength, statisticName, valuesName);
+        appendStringFormattedDelimiter(stringBuilder, maxNameLength, maxValuesLength);
         for (int i = 0; i < tableLength; i++) {
-            stringBuilder
-                .append("|")
-                .append(names[i])
-                .repeat(" ", maxNameLength - names[i].length())
-                .append("│")
-                .append(values[i])
-                .repeat(" ", maxValuesLength - values[i].length())
-                .append("│")
-                .append("\n");
+            appendStringFormattedDataLine(stringBuilder, maxNameLength, maxValuesLength, names[i], values[i]);
             if (i != lastLine) {
-                stringBuilder
-                    .append("├")
-                    .repeat("─", maxNameLength)
-                    .append("┼")
-                    .repeat("─", maxValuesLength)
-                    .append("┤")
-                    .append("\n");
-            } else {
-                stringBuilder
-                    .append("└")
-                    .repeat("─", maxNameLength)
-                    .append("┴")
-                    .repeat("─", maxValuesLength)
-                    .append("┘")
-                    .append("\n");
+                appendStringFormattedDelimiter(stringBuilder, maxNameLength, maxValuesLength);
             }
         }
+        appendStringFormattedLastLine(stringBuilder, maxNameLength, maxValuesLength);
         return stringBuilder.toString();
     }
 
@@ -128,17 +75,8 @@ public class FormatUtils {
 
         long tableLength = names.length;
 
-        int maxNameLength = Arrays.stream(names)
-            .map(String::length)
-            .max(Comparator.comparingInt(a -> a))
-            .orElse(0);
-        maxNameLength = Math.max(maxNameLength, statisticName.length());
-
-        int maxValuesLength = Arrays.stream(values)
-            .map(String::length)
-            .max(Comparator.comparingInt(a -> a))
-            .orElse(0);
-        maxValuesLength = Math.max(maxValuesLength, valuesName.length());
+        int maxNameLength = getMaxNameLength(names, statisticName);
+        int maxValuesLength = getMaxValuesLength(values, valuesName);
 
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder
@@ -176,5 +114,82 @@ public class FormatUtils {
         }
 
         return stringBuilder.toString();
+    }
+
+    private static void appendStringFormattedLastLine(StringBuilder stringBuilder,
+        int maxNameLength, int maxValuesLength) {
+        stringBuilder
+            .append("└")
+            .repeat("─", maxNameLength)
+            .append("┴")
+            .repeat("─", maxValuesLength)
+            .append("┘")
+            .append("\n");
+    }
+
+    private static void appendStringFormattedDataLine(StringBuilder stringBuilder,
+        int maxNameLength, int maxValuesLength,
+        String name, String value) {
+        stringBuilder
+            .append("|")
+            .append(name)
+            .repeat(" ", maxNameLength - name.length())
+            .append("│")
+            .append(value)
+            .repeat(" ", maxValuesLength - value.length())
+            .append("│")
+            .append("\n");
+    }
+
+    private static void appendStringFormattedDelimiter(StringBuilder stringBuilder,
+        int maxNameLength, int maxValuesLength) {
+        stringBuilder
+            .append("├")
+            .repeat("─", maxNameLength)
+            .append("┼")
+            .repeat("─", maxValuesLength)
+            .append("┤")
+            .append("\n");
+    }
+
+    private static void appendStringFormattedTitle(StringBuilder stringBuilder,
+        int maxNameLength, int maxValuesLength,
+        String statisticName, String valuesName) {
+        stringBuilder
+            .append("│")
+            .append(statisticName)
+            .repeat(" ", maxNameLength - statisticName.length())
+            .append("│")
+            .append(valuesName)
+            .repeat(" ", maxValuesLength - valuesName.length())
+            .append("│")
+            .append("\n");
+    }
+
+    private static void appendStringFormattedFirstLine(StringBuilder stringBuilder,
+        int maxNameLength, int maxValuesLength) {
+        stringBuilder
+            .append("┌")
+            .repeat("─", maxNameLength)
+            .append("┬")
+            .repeat("─", maxValuesLength)
+            .append("┐")
+            .append("\n");
+    }
+
+    private static int getMaxNameLength(String[] names, String statisticName) {
+        int maxNameLength = Arrays.stream(names)
+            .map(String::length)
+            .max(Comparator.comparingInt(a -> a))
+            .orElse(0);
+        return Math.max(maxNameLength, statisticName.length());
+    }
+
+    private static int getMaxValuesLength(String[] values, String valuesName) {
+        int maxValuesLength = Arrays.stream(values)
+            .map(String::length)
+            .max(Comparator.comparingInt(a -> a))
+            .orElse(0);
+        return Math.max(maxValuesLength, valuesName.length());
     }
 }
